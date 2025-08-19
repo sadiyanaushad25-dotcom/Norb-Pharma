576dba67cb77f953f258606fc1228daab7ead490
sadiyanaushad25-dotcom <sadiya.naushad25@gmail.com>
2025-08-19 16:48:25 +0400

    service card update

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -1,13 +1,28 @@
 import TextReveal from '../TextReveal';
 import './ServiceCard.css';
 import { GiCheckMark } from "react-icons/gi";
+import { motion } from "framer-motion";
 
 function ServiceCard({ iconSrc, name, description }) {
   return (
     <div>
     <div className="service-box">
       <div className="icon-circle">
-        <div className="icon-image">{iconSrc}</div>
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          className="icon-image"
+          variants={{
+            hidden: { y: "-120%", opacity: 0 },
+            visible: {
+              y: 0,
+              opacity: 1,
+              transition: { delay: 0.2, duration: 0.8, ease: "easeInOut" }
+            },
+          }}
+        >
+          <div >{iconSrc}</div>
+        </motion.div>
       </div>
       <div className="name-text">{name}</div>
       <div className="desc-text">
@@ -15,7 +30,7 @@ function ServiceCard({ iconSrc, name, description }) {
         <TextReveal
         as="p"
         text={description}
-        type="wave"
+        type="reverse"
         delay={0.04}
       />
     </div>
