e2f073beef9a19aa7ed8cefbc91c3bd6419aa151
sadiyanaushad25-dotcom <sadiya.naushad25@gmail.com>
2025-08-29 17:27:48 +0400

    change

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
 
   useEffect(() => {
     if (loaded) {
-      const timer = setTimeout(() => setReady(true), 1000);
+      const timer = setTimeout(() => setReady(true), 3000);
       return () => clearTimeout(timer);
     }
   }, [loaded]);
