a957008dee6f3621e6935a02c4a2b4772f2aabcf
sadiyanaushad25-dotcom <sadiya.naushad25@gmail.com>
2025-09-16 15:40:58 +0400

    working

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,13 +1,24 @@
 import ServiceCard from '../components/ServiceCard/ServiceCard'
 import { service1, service2, service3, service4, vision } from '../constants/keywords';
+import { motion } from "framer-motion";
 import { HiBuildingOffice } from "react-icons/hi2";
 import { FaTachographDigital } from "react-icons/fa6";
 import { HiTruck } from "react-icons/hi2";
 import { FaFileSignature } from "react-icons/fa";
 import './Services.css'
 import TextReveal from '../components/TextReveal';
+import useIsMobile from '../Hooks/useIsMobile';
 
 export default function Services() {
+  const isMobile = useIsMobile(768);
+
+  const services = [
+    { icon: <HiBuildingOffice size={25} />, description: service1 },
+    { icon: <FaTachographDigital size={25} />, description: service2 },
+    { icon: <HiTruck size={25} />, description: service3 },
+    { icon: <FaFileSignature size={25} />, description: service4 },
+  ];
+
   return (
     <div id="services" style={{color: "#fff", position: "relative", overflow:'hidden' }}>
       <div className='services-wrapper'>
@@ -20,28 +31,30 @@ export default function Services() {
         className="services-wrapper-h2"
       />
       <p className='vision'>{vision}</p>
+      {isMobile ? 
+    <div className="mobile-services">
+      {services.map((service, index) => (
+        <div
+          key={index}
+          className={`service-box-mobile ${index < 2 ? "scroll-right" : "scroll-left"}`}
+        >
+          <div className="service-icon">{service.icon}</div>
+          <div className="service-text">{service.description}</div>
+        </div>
+      ))}
+    </div>
+      :
         <section className='service-cards'>
+      {services.map((service, index) => (
         <ServiceCard
-          iconSrc={<HiBuildingOffice size={25} />}
+          key={index}
+          iconSrc={service.icon}
           name=""
-          description={service1}
-        />         
-        <ServiceCard
-          iconSrc={<FaTachographDigital size={25} />}
-          name=""
-          description={service2}
-        />    
-        <ServiceCard
-          iconSrc={<HiTruck size={25} />}
-          name=""
-          description={service3}
+          description={service.description}
         />
-        <ServiceCard
-          iconSrc={<FaFileSignature size={25} />}
-          name=""
-          description={service4}
-        />
+      ))}
         </section>
+}
       </div>
     </div>
   );
