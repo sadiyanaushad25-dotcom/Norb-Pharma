76b44acfba7b9e99674dd8172cbe0ada22fe0a30
sadiyanaushad25-dotcom <sadiya.naushad25@gmail.com>
2025-10-07 17:25:09 +0400

    images

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,10 +4,10 @@ import './About.css'
 
 export default function About() {
   const employees = [
-  { name: "Kahlid Baig", degree: "Certified Pharmacist & Regulatory Affairs" },
-  { name: "Zara Khanam", degree: "Biomedical Engineer & Regulatory Affairs Specialis" },
-  { name: "Gowher Mirza", degree: "Mechanical Engineer & DG certified" },
-  { name: "Sufyan baig", degree: "Digital marketing expert" },
+  { name: "Kahlid Baig", degree: "Certified Pharmacist & Regulatory Affairs", img:"/kahlid_baig.webp" },
+  { name: "Zara Khanam", degree: "Biomedical Engineer & Regulatory Affairs Specialis", img:"/zara_khanam.webp" },
+  { name: "Gowher Mirza", degree: "Mechanical Engineer & DG certified", img:"/gowher_mirza.webp" },
+  { name: "Sufyan Baig", degree: "Digital marketing expert", img:"/sufyan_baig.webp" },
 ];
   return (
     <div>
@@ -30,7 +30,16 @@ export default function About() {
         {employees.map((emp, index) => (
           <div className="team-card" key={index}>
             <div className="initial-circle">
-              {emp.name.charAt(0)}
+              {/* {emp.name.charAt(0)} */}
+               <img
+                src={emp.img}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  borderRadius: "50%",
+                  objectFit: "cover",
+                }}
+              />
             </div>
               <TextReveal
                 as="h3"
